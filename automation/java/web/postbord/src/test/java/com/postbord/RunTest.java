158a5f6ce2bfde8ee0b406055481f69d33ec359d
BEE AUX <yomi.folami@beeaux.com>
2014-03-06 11:44:06 +0000

    Update RunTest.java
    configured runtest with cucumber & junit options

--- automation/java/web/postbord/src/test/java/com/postbord/RunTest.java
+++ automation/java/web/postbord/src/test/java/com/postbord/RunTest.java
@@ -1,5 +1,7 @@
 package com.postbord;
 
 
+@RunWith(Cucumber.class)
+@CucumberOptions(format = {"html:target/reports/cucumber/html", "json:target/reports/cucumber/result.json"})
 public class RunTest {
 }
